265db27f9cf4175635a599f1a8374dbae7e32bf4
sandisodev <sandisodev@gmail.com>
2024-09-14 19:14:39 +0200

    done with the landing , filtering and products designs, only left with using js to dynamically render products then create the shipping section

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import products from "./products";
+
 const div = document.querySelector('.slogan'); 
 const text = `Where future meets fashion...`;
 
@@ -21,19 +23,31 @@ typingEffect(div, text);
 const welcomingVideo = () => {
     // Select existing overlay and video elements
     const overlay = document.querySelector('.overLay');
+    const landingOverlay = document.querySelector('.landingOverlay');
     const logbg = document.querySelector('.bglog');
+    const bod = document.querySelector('#hid');
 
     // Show the overlay and video when the page loads
     setTimeout(() => {
         overlay.style.display = "block";
         logbg.style.display = "block";
+        landingOverlay.style.display = "none"
+        bod.style.overflow = "hidden";
     }, 500); // Small delay for smooth appearance
 
     // Hide the overlay and video after 20 seconds
     setTimeout(() => {
         overlay.style.display = "none";
         logbg.style.display = "none";
-    }, 12000); // 20 seconds in milliseconds (20000ms)
+        landingOverlay.style.display = "block"
+        bod.style.overflow = "scroll";
+
+        
+     // 20 seconds in milliseconds (20000ms)
+    }, 20000); 
 };
 
 document.addEventListener('DOMContentLoaded', welcomingVideo);
+
+
+//RENDER ITEMS DYNAMIC FROM THE DATA I HAVE BUILT SO THAT I CAN ALSO BE ABLE TO CREAT A FILTER FUNCTION
---
+++ products.js
@@ -0,0 +1,184 @@
+//TSHIRTS 
+import t1 from "./assests/ciaga-tshirts/t1.png";
+import t2 from "./assests/ciaga-tshirts/t2.png";
+import t3 from "./assests/ciaga-tshirts/t3.png";
+import t4 from "./assests/ciaga-tshirts/t4.png";
+import t5 from "./assests/ciaga-tshirts/t5.png";
+import t6 from "./assests/ciaga-tshirts/t6.png";
+import t7 from "./assests/ciaga-tshirts/t7.png";
+import t8 from "./assests/ciaga-tshirts/t8.png";
+import t9 from "./assests/ciaga-tshirts/t9.png";
+import t10 from "./assests/ciaga-tshirts/t10.png";
+
+//HOODIES 
+import h1 from "./assests/ciaga-hoodies/h1.png";
+import h2 from "./assests/ciaga-hoodies/h2.png";
+import h3 from "./assests/ciaga-hoodies/h3.png";
+import h4 from "./assests/ciaga-hoodies/h4.png";
+import h5 from "./assests/ciaga-hoodies/h5.png";
+import h6 from "./assests/ciaga-hoodies/h6.png";
+import h7 from "./assests/ciaga-hoodies/h7.png";
+import h8 from "./assests/ciaga-hoodies/h8.png";
+
+//SHOES 
+import s1 from "./assests/ciaga-shoes/s1.png";
+import s2 from "./assests/ciaga-shoes/s2.png";
+import s3 from "./assests/ciaga-shoes/s3.png";
+import s4 from "./assests/ciaga-shoes/s4.png";
+import s5 from "./assests/ciaga-shoes/s5.png";
+import s6 from "./assests/ciaga-shoes/s6.png";
+
+let products = [
+    {
+        id: 1,
+        category: "tshirts",
+        img: t1,
+        price: 5000
+    },
+    {
+        id: 2,
+        category: "tshirts",
+        img: t2,
+        price: 3000
+    },
+    {
+        id: 3,
+        category: "tshirts",
+        img: t3,
+        price: 7000
+    },
+    {
+        id: 4,
+        category: "tshirts",
+        img: t4,
+        price: 2500
+    },
+    {
+        id: 5,
+        category: "tshirts",
+        img: t5,
+        price: 5000
+    },
+    {
+        id: 6,
+        category: "tshirts",
+        img: t6,
+        price: 4300
+    },
+    {
+        id: 7,
+        category: "tshirts",
+        img: t7,
+        price: 9000
+    },
+    {
+        id: 8,
+        category: "tshirts",
+        img: t8,
+        price: 5000
+    },
+    {
+        id: 9,
+        category: "tshirts",
+        img: t9,
+        price: 5000
+    },
+    {
+        id: 10,
+        category: "tshirts",
+        img: t10,
+        price: 9000
+    },
+    {
+        id: 11,
+        category: "hoodies",
+        img: h1,
+        price: 10000
+    },
+    {
+        id: 12,
+        category: "hoodies",
+        img: h2,
+        price: 9000
+    },
+    {
+        id: 13,
+        category: "hoodies",
+        img: h3,
+        price: 11000
+    },
+    {
+        id: 14,
+        category: "hoodies",
+        img: h4,
+        price: 8000
+    },
+    {
+        id: 15,
+        category: "hoodies",
+        img: h5,
+        price: 6200
+    },
+    {
+        id: 16,
+        category: "hoodies",
+        img: h6,
+        price: 3000
+    },
+    {
+        id: 17,
+        category: "hoodies",
+        img: h7,
+        price: 10000
+    },
+    {
+        id: 18,
+        category: "hoodies",
+        img: h8,
+        price: 2000
+    },
+    {
+        id: 19,
+        category: "shoes",
+        img: s1,
+        price: 22000
+    },
+    {
+        id: 20,
+        category: "shoes",
+        img: s2,
+        price: 13000
+    },
+    {
+        id: 21,
+        category: "shoes",
+        img: s3,
+        price: 22000
+    },
+    {
+        id: 22,
+        category: "shoes",
+        img: s3,
+        price: 10000
+    },
+    {
+        id: 23,
+        category: "shoes",
+        img: s4,
+        price: 12000
+    },
+    {
+        id: 24,
+        category: "shoes",
+        img: s5,
+        price: 4000
+    },
+    {
+        id: 25,
+        category: "shoes",
+        img: s6,
+        price: 20000
+    },
+]
+
+export default products
