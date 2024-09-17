5192ba92e94e4af455b6f2509584a6c2aaea9635
sandisodev <sandisodev@gmail.com>
2024-09-18 00:46:27 +0200

    added all features, tested and ready to deploy

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const cartBtn = document.querySelector('.cartBtn');
 const backBtn = document.querySelector('.backBtn');
 const productContainer = document.querySelector('.products');
 const shipping = document.querySelector('.shipping');
+const nav = document.querySelector('.nav');
+
+//LOCALSTORAGE KEY 
+const LOCAL_KEY = "mveli211106";
 
 const typingEffect = (element, text, idx = 0) => {
     if (idx === 0) {
@@ -42,6 +46,7 @@ const welcomingVideo = () => {
         landingOverlay.style.display = "none"
         productContainer.style.display = "none"
         bod.style.overflow = "hidden";
+        nav.style.display = "none";
     }, 500); // Small delay for smooth appearance
 
     // Hide the overlay and video after 8 seconds
@@ -51,7 +56,8 @@ const welcomingVideo = () => {
         landingOverlay.style.display = "block";
         productContainer.style.display = "flex";
         bod.style.overflow = "scroll";
-    }, 8000); 
+        nav.style.display = "block";
+    }, 6000); 
 };
 
 
@@ -59,21 +65,25 @@ const welcomingVideo = () => {
 cartBtn.addEventListener('click', () => {
     shipping.style.display = "grid";
     productContainer.style.display = "none";
+    nav.style.display = "none";
+});
 
-}); 
 backBtn.addEventListener('click', () => {
     shipping.style.display = "none";
     productContainer.style.display = "flex";
+    nav.style.display = "block";
+    console.log("working")
 });
 
 //DISPLAY ITEMS DYNAMIC FROM THE DATA - done
-//ADD ITEMS TO CART
-//DISPLAY ITEMS TO CART 
+//ADD ITEMS TO CART - done
+//DISPLAY ITEMS TO CART - done 
+//UPDATE CART QUANTITY IN HOME PAGE - done 
 //FILTERING FEATURE - done 
-//SAVE ITEMS ON CART IF PAGE REFRESHES OR USER LEAVES THE SITE FOR A MOMENT 
-//DELETE ITEMS FROM CART 
-//INCREASE OR DECREASE QUANTITY ALONG WITH THE PRICE OF THE PRODUCT 
-//CALCULATE TOTAL
+//SAVE ITEMS ON CART IF PAGE REFRESHES OR USER LEAVES THE SITE FOR A MOMENT - done 
+//DELETE ITEMS FROM CART - done 
+//INCREASE OR DECREASE QUANTITY ALONG WITH THE PRICE OF THE PRODUCT - done 
+//CALCULATE TOTAL - done 
 
 //USING THIS FUNCTION TO SHUFFLE MY PRODUCTS IN THE HOME PAGE SINCE MY DATA IS SORTED TO RELEVANCE
 //BY USING THE SORT METHOD WITH A RANDOM COMPARISON.
@@ -155,35 +165,170 @@ document.querySelectorAll('.options div').forEach(option => {
     });
 });
 
+
+//SAVING THE CART ITEMS TO LOCAL STORAGE FOR PERSISTANCE
+const saveItem = () => {
+    let toStringItems = JSON.stringify(cartItems);
+    localStorage.setItem(LOCAL_KEY, toStringItems);
+}
+
+//RETRIEVING ITEMS FROM LOCAL STORAGE IF THERE ARE 
+const loadItemsCartItems = () => {
+    let savedItems = localStorage.getItem(LOCAL_KEY);
+    if (savedItems) cartItems = JSON.parse(savedItems);
+    renderCartItems();
+    displayAmountCart()
+};
+
 const addToCartFunc = (idx) => {
 
     //check if item exists in the cart 
     const existingCartItem = cartItems.find(item => item.id === idx);
 
     if(existingCartItem){
-        existingCartItem.quantity +=1;
+        existingCartItem.quantity += 1;
+
     }else{
+        //FETCHING PRODUCT DETAILS 
+        const product = products[idx];
         cartItems.push({
             id:idx, 
-            quantity: 1
+            quantity: 1,
+            price: product.price,
+            img: product.img
         });
     }
 
-    renderCartItems()
-
+    saveItem();
+    renderCartItems();
+    displayAmountCart();
 }
 
 const renderCartItems = () => {
-    console.log("HERE ARE THE CART ITEMS:"); 
+  
+    // Targeting the container for items in the cart to append to
+    const itemsContainer = document.querySelector('.itemsContainer');
+    itemsContainer.innerHTML = null;
 
-    cartItems.forEach(item => {
-        console.log(  `id:${item.id}, price:${item.price}`)
+    for (const [idx, item] of Object.entries(cartItems)) {
+        // Create the elements which will be appended
+        const itemDesc = document.createElement('div');
+        itemDesc.className = "itemDescription";
+
+        const prod = document.createElement('div');
+        prod.className = "item"; 
+
+        const prodImg = document.createElement('img');
+        prodImg.src = item.img;
+
+        const addBtn = document.createElement('button');
+        addBtn.className = "add";
+        addBtn.textContent = "Add quantity";
+        addBtn.onclick = () => addItemQuant(item);
+
+        const minusBtn = document.createElement('button');
+        minusBtn.className = "minus";
+        minusBtn.textContent = "Minus quantity";
+        minusBtn.onclick = () => minusItemQuant(item);
+
+        const text = document.createElement('p');
+        text.classList = "quantity";
+        text.textContent = `Quantity: ${item.quantity}`;
+
+        const pric = document.createElement('p');
+        pric.textContent = `Price: R${item.price * item.quantity}`;
+
+        itemDesc.appendChild(text);
+        itemDesc.appendChild(addBtn);
+        itemDesc.appendChild(minusBtn);
+        itemDesc.appendChild(pric);
+
+        prod.appendChild(prodImg);
+        prod.appendChild(itemDesc);
+
+        itemsContainer.appendChild(prod);
     }
-    )
-}
+
+    // Update the total every time a new item is added
+    calculateTotal();
+};
+
+
+//INCREMENTINT 
+const addItemQuant = (item) => {
+    const existItem = cartItems.find(prod => prod.id === item.id);
+    if (existItem) {
+        existItem.quantity++;
+        saveItem();
+        renderCartItems();
+         // Update total whenever quantity changes
+        calculateTotal(); 
+        displayAmountCart(cartItems);
+    }
+};
+
+
+//DECREMENT OR DELETE
+const minusItemQuant = (item) => {
+    const existItem = cartItems.find(prod => prod.id === item.id);
+    if (existItem && existItem.quantity > 1) {
+        existItem.quantity--;
+        saveItem();
+        renderCartItems();
+        calculateTotal(); 
+
+    //IF THE PRODUCT DOES EXIST AND THE QUANTITY IS 1 DELETE IT IF IT REACHES ZERO
+    } else if (existItem && existItem.quantity === 1) {
+        // remove the item if quantity reaches 0
+        cartItems = cartItems.filter(prod => prod.id !== item.id);
+        saveItem();
+        renderCartItems();
+        calculateTotal();
+        displayAmountCart(cartItems)
+    }
+};
+
+
+const calculateTotal = () => {
+    const totalContainer = document.querySelector('.totalContainer');
+    //FORMULA TO CALCULATE THE TOTAL AMOUNT BY SUMMING UP THE QUANTITY WITH PRICE AND THE LENGTH OF THE ARRAY
+    let totalAmount = cartItems.reduce((acc, item) => acc + (item.price * item.quantity), 0);
+    
+    const totalText = document.querySelector('.totalAmount');
+    if (!totalText) {
+        const newTotalText = document.createElement('p');
+        newTotalText.className = "totalAmount";
+        totalContainer.appendChild(newTotalText);
+    } else {
+        totalText.textContent = `Total: R${totalAmount}`;
+    }
+    
+};
+
+//DISPLAYING AMOUNT OF ITEMS IN CART IN HOME PAGE 
+const displayAmountCart = () => {
+    const quant = document.querySelector('.quantity');
+
+    // Check if cartItems exists and has items
+    const totalQuantity = cartItems.length > 0 
+        ? cartItems.reduce((total, item) => total + item.quantity, 0) 
+        : 0;
+
+    quant.textContent = totalQuantity;
+
+    // Optionally hide the quantity element if it's 0
+    if (totalQuantity === 0) {
+        quant.style.display = 'none';
+    } else {
+        quant.style.display = 'inline';  // Make sure it shows when quantity is more than 0
+    }
+};
+
 document.addEventListener('DOMContentLoaded', () => {
 welcomingVideo();
 renderProducts();
+displayAmountCart();
+loadItemsCartItems();
 }
 );
 
--- products.js
+++ products.js
@@ -5,121 +5,141 @@ let products = [
         id: 2,
         category: "tshirts",
         img: './assets/ciaga-tshirts/t2.png',
-        price: 3000
+        price: 3000,
+        quantity: 1
     },
     {
         id: 3,
         category: "tshirts",
         img: './assets/ciaga-tshirts/t3.png',
-        price: 7000
+        price: 7000,
+        quantity: 1
     },
     {
         id: 4,
         category: "tshirts",
         img: './assets/ciaga-tshirts/t4.png',
-        price: 2500
+        price: 2500,
+        quantity: 1
     },
     {
         id: 5,
         category: "tshirts",
         img: './assets/ciaga-tshirts/t5.png',
-        price: 5000
+        price: 5000,
+        quantity: 1
     },
     {
         id: 6,
         category: "tshirts",
         img: './assets/ciaga-tshirts/t6.png',
-        price: 4300
+        price: 4300,
+        quantity: 1
     },
     {
         id: 7,
         category: "tshirts",
         img: './assets/ciaga-tshirts/t7.png',
-        price: 9000
+        price: 9000,
+        quantity: 1
     },
     {
         id: 8,
         category: "tshirts",
         img: './assets/ciaga-tshirts/t8.png',
-        price: 5000
+        price: 5000,
+        quantity: 1
     },
     {
         id: 9,
         category: "tshirts",
         img: './assets/ciaga-tshirts/t9.png',
-        price: 5000
+        price: 5000,
+        quantity: 1
     },
     {
         id: 10,
         category: "tshirts",
         img: './assets/ciaga-tshirts/t10.png',
-        price: 9000
+        price: 9000,
+        quantity: 1
     },
     {
         id: 12,
         category: "hoodies",
         img: './assets/ciaga-hoodies/h2.png',
-        price: 9000
+        price: 9000,
+        quantity: 1
     },
     {
         id: 13,
         category: "hoodies",
         img: './assets/ciaga-hoodies/h3.png',
-        price: 11000
+        price: 11000,
+        quantity: 1
     },
     {
         id: 14,
         category: "hoodies",
         img: './assets/ciaga-hoodies/h4.png',
-        price: 8000
+        price: 8000,
+        quantity: 1
     },
     {
         id: 15,
         category: "hoodies",
         img: './assets/ciaga-hoodies/h5.png',
-        price: 6200
+        price: 6200,
+        quantity: 1
     },
     {
         id: 16,
         category: "hoodies",
         img: './assets/ciaga-hoodies/h6.png',
-        price: 3000
+        price: 3000,
+        quantity: 1
     },
     {
         id: 17,
         category: "hoodies",
         img: './assets/ciaga-hoodies/h7.png',
-        price: 10000
+        price: 10000,
+        quantity: 1
     },
     {
         id: 18,
         category: "hoodies",
         img: './assets/ciaga-hoodies/h8.png',
-        price: 2000
+        price: 2000,
+        quantity: 1
     },
     {
         id: 19,
         category: "shoes",
         img: './assets/ciaga-shoes/s1.png',
-        price: 22000
+        price: 22000,
+        quantity: 1
     },
     {
         id: 20,
         category: "shoes",
         img: './assets/ciaga-shoes/s2.png',
-        price: 13000
+        price: 13000,
+        quantity: 1
     },
     {
         id: 21,
         category: "shoes",
         img: './assets/ciaga-shoes/s3.png',
-        price: 22000
+        price: 22000,
+        quantity: 1
     },
     {
         id: 24,
         category: "shoes",
         img: './assets/ciaga-shoes/s6.png',
-        price: 4000
+        price: 4000,
+        quantity: 1
     },
 ]
 
