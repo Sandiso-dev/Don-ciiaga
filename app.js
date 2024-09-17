481659277d64cbc14158a1fa7261841bbf89be51
sandisodev <sandisodev@gmail.com>
2024-09-18 00:59:47 +0200

    added footer

--- app.js
+++ app.js
@@ -325,7 +325,7 @@ const displayAmountCart = () => {
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-welcomingVideo();
+//welcomingVideo();
 renderProducts();
 displayAmountCart();
 loadItemsCartItems();
